f5697c007d60fe6f6cb478971ed0c034c9533ff4
Ianhw <43971613+Ianhw@users.noreply.github.com>
2020-02-11 14:38:59 -0600

    Add files via upload

--- average_scores.py
+++ average_scores.py
@@ -1,4 +1,4 @@
-
+import sys
 """
 Program: average_scores.py
 Author: ian Waddell
@@ -10,31 +10,64 @@ This program takes the average score of a person then output it.
 def average():
     # Get input for scores
     # declare variables, use score1, score2, score3 in calculation
+
+    print('Enter first score:')
+    one = input()
+
+    print('Enter second score:')
+    two = input()
+
+    print('Enter third score:')
+    three = input()
+
+    numone = int(one)
+    if (numone < 0):
+        print("Bad input, good-bye!")
+        raise ValueError
+        return ValueError
+    try:
+        numone < 0
+    except :
+        print("Do not enter a negative")
+        raise ValueError
+
+
+    numtwo = int(two)
+    if (numtwo < 0):
+        print("Bad input, good-bye!")
+        raise ValueError
+    try:
+        numtwo < 0
+    except :
+        print("Do not enter a negative")
+        raise ValueError
+    numthree = int(three)
+    if (numthree < 0):
+        print("Bad input, good-bye!")
+        raise ValueError
+    try:
+        numthree < 0
+    except:
+        print("Do not enter a negative")
+        raise ValueError
     global average
-    average = int(one) + int(two) + int(three)
+    average = int(numone) + int(numtwo) + int(numthree)
     average = average/3
-    average = str(round(average, 2))
+    average = int(average)
     print(average)
     return average
 
 
 
 if __name__ == '__main__':
+
+
     print('Enter first name:')
     fn = input()
     print('Enter last name:')
     ln = input()
     print('Enter age:')
     age = input()
-    print('Enter first score:')
-    one = input()
-    int(one)
-    print('Enter second score:')
-    two = input()
-    int(two)
-    print('Enter third score:')
-    three = input()
-    int(three)
 
     average()
 
--- test_average_scores.py
+++ test_average_scores.py
@@ -1,12 +1,16 @@
 import unittest
+import unittest.mock as mock
+import sys
 from format_output import average_scores
 
 class FunctionTestCase(unittest.TestCase):
     def test_average(self):
-        self.assertEqual(3, average_scores.average( 2, 3, 4))
+        with mock.patch('builtins.input', side_effect=[2, 3, 4]):
+            assert average_scores.average() == 3
 
-
-
+    def test_average_exception(self):
+        with self.assertRaises(ValueError):
+            average_scores.average(-90, 89, 78)
 
 if __name__ == '__main__':
     unittest.main()
